5d1d5db3068994ac54251c014500656aaaac766c
Appan Roy <appan12345@gmail.com>
2020-09-17 21:19:21 +0530

    final commit

--- LearnProtractor/config/conf.js
+++ LearnProtractor/config/conf.js
@@ -1,11 +1,5 @@
 // An example configuration file.
-var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
 var HtmlReporter = require('protractor-beautiful-reporter');
-
-var reporter = new HtmlScreenshotReporter({
-  dest: './target/jasmine-results',
-  filename: 'my-report.html'
-});
 
 exports.config = {
   directConnect: true,
@@ -28,88 +22,18 @@ exports.config = {
     defaultTimeoutInterval: 30000
   },
 
-  // Setup the report before any tests start
-  beforeLaunch: function () {
-    return new Promise(function (resolve) {
-      reporter.beforeLaunch(resolve);
-    });
-  },
-
   // Assign the test reporter to each running instance
   onPrepare: function () {
-    jasmine.getEnv().addReporter(reporter);
-
+    
     var AllureReporter = require('jasmine-allure-reporter');
     jasmine.getEnv().addReporter(new AllureReporter({
       resultsDir: './target/allure-results'
-    }));
-
-    var jasmineReporters = require('jasmine-reporters');
-    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
-      consolidateAll: true,
-      savePath: './',
-      filePrefix: 'xmlresults'
     }));
 
     jasmine.getEnv().addReporter(new HtmlReporter({
       baseDirectory: 'target/beautiful-results/screenshots'
     }).getJasmine2Reporter());
 
-    var fs = require('fs-extra');
-
-    fs.emptyDir('screenshots/', function (err) {
-      console.log(err);
-    });
-
-    jasmine.getEnv().addReporter({
-      specDone: function (result) {
-        if (result.status == 'failed') {
-          browser.getCapabilities().then(function (caps) {
-            var browserName = caps.get('browserName');
-
-            browser.takeScreenshot().then(function (png) {
-              var stream = fs.createWriteStream('screenshots/' + browserName + '-' + result.fullName + '.png');
-              stream.write(new Buffer(png, 'base64'));
-              stream.end();
-            });
-          });
-        }
-      }
-    });
-  },
-
-  // Close the report after all tests finish
-  afterLaunch: function (exitCode) {
-    return new Promise(function (resolve) {
-      reporter.afterLaunch(resolve.bind(this, exitCode));
-    });
-  },
-
-  //HTMLReport called once tests are finished
-  onComplete: function () {
-    var browserName, browserVersion;
-    var capsPromise = browser.getCapabilities();
-
-    capsPromise.then(function (caps) {
-      browserName = caps.get('browserName');
-      browserVersion = caps.get('version');
-      platform = caps.get('platform');
-
-      var HTMLReport = require('protractor-html-reporter-2');
-
-      testConfig = {
-        reportTitle: 'Protractor Test Execution Report',
-        outputPath: './',
-        outputFilename: 'ProtractorTestReport',
-        screenshotPath: './screenshots',
-        testBrowser: browserName,
-        browserVersion: browserVersion,
-        modifiedSuiteName: false,
-        screenshotsOnlyOnFailure: true,
-        testPlatform: platform
-      };
-      new HTMLReport().from('xmlresults.xml', testConfig);
-    });
   }
 
 };
